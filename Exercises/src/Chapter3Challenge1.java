5883680d39b276fe3c0f72998d88a201cfd481c7
Carlos Adolfo <cardolfo@gmail.com>
2022-09-26 21:54:49 -0300

    Adding Exercices

---
+++ Exercises/src/Chapter3Challenge1.java
@@ -0,0 +1,30 @@
+
+public class Chapter3Challenge1{
+
+    static class Person{
+        public final String name;
+        public final Integer age;
+
+        public Person(String name, Integer age) {
+            this.name = name;
+            this.age = age;            
+        }        
+    }
+
+    static class Car{
+        public final String make;
+        public final String color;
+        public final Float price;
+
+        public Car(String make, String color, Float price){
+            this.make = make;
+            this.color = color;
+            this.price = price;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
